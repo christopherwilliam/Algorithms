de08176ed309e91f994474ee03a1b90d310ef724
Christopher McIlraith <c.w.mcilraith@gmail.com>
2020-01-20 16:34:24 -0800

    Build isPrime and primeFactors  methods
    iteratively solve both algorithms
    use isPrime inside primeFactors
    isPrime returns a boolean, primeFactors returns an object with indexed prime factors listed from largest to smallest

---
+++ src/algorithms/primeFinder.js
@@ -0,0 +1,19 @@
+export default function isPrime(number) {
+    var start = Math.ceil(number / 2);
+    for (let i = start; i >= 2; i--) {
+        if (!(number%i)) {
+            return false;
+        }
+    }
+    return true;
+}
+export function primeFactors(number) {
+    var start = Math.ceil(number / 2);
+    var factors = {};
+    for (let factor = start; factor > 1; factor--) {
+        if (!(number%factor) && isPrime(factor)) {
+            factors[Object.keys(factors).length + 1] = factor;
+        }
+    }
+    return factors;
+}
